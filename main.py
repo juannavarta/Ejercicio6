8f8157e26001260333bd284b3a7c70350a9966a4
juannavarta <130484568+juannavarta@users.noreply.github.com>
2023-05-07 16:14:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+import csv
+from classViajero import ViajeroFrecuente
+if __name__ == "__main__":
+    viajeros = []
+    archivo = open('C://Users//csv//Viajeros.csv', 'r')
+    reader = csv.reader(archivo, delimiter=";")
+    for fila in reader:
+        num = int(fila[0])
+        dni = fila[1]
+        nom = fila[2]
+        ape = fila[3]
+        millas = int(fila[4])
+        viaje = ViajeroFrecuente(num, dni, nom, ape, millas)
+        viajeros.append(viaje)
+    archivo.close()
+    i=0
+    while i==0:
+        print("Menu")
+        print("1. Consultar Millas")
+        print("2. Acumular Millas")
+        print("3. Canjear Millas")
+        print("4. Determinar viajero/s con mayor cantidad de millas acumuladas")
+        print("5. Lo mismo que el 2, otro metodo")
+        print("5. Lo mismo que el 3, otro metodo")
+        print("0. Cerrar")
+        opcion = int(input())
+        if opcion==0:
+            exit()
+        elif opcion==1:
+            j=0
+            numV = int(input("Ingrese numero de viajero a consultar: "))
+            while j<=len(viajeros) and numV != viajeros[j].getNumero():
+                j+=1
+            if j > len(viajeros):
+                print("Numero incorrecto")
+            else:
+                print("El viajero con numero {} tiene {} millas".format(viajeros[j].getNumero(), viajeros[j].getMillas()))
+        elif opcion==2:
+            j=0
+            numV = int(input("Ingrese numero de viajero a acumular millas: "))
+            while j<=len(viajeros) and numV != viajeros[j].getNumero():
+                j+=1
+            if j > len(viajeros):
+                print("Numero incorrecto")
+            else:
+                mil = int(input("Ingrese la cantidad de millas a acumular: "))
+                print("El viajero con numero {} ahora tiene {} millas".format(viajeros[j].getNumero(), viajeros[j].acumularMillas(mil)))
+        elif opcion==3:
+            j=0
+            numV = int(input("Ingrese numero de viajero a canjear millas: "))
+            while j<=len(viajeros) and numV != viajeros[j].getNumero():
+                j+=1
+            if j > len(viajeros):
+                print("Numero incorrecto")
+            else:
+                mil = int(input("Ingrese la cantidad de millas a canjear: "))
+                if mil <= viajeros[j].getMillas():
+                    print("El viajero con numero {} ahora tiene {} millas".format(viajeros[j].getNumero(), viajeros[j].canjearMillas(mil)))
+                else:
+                    print("La cantidad de millas seleccionadas no alcanzan para realizar el canje")
+            print(max.getMillas)
+        elif opcion==4:
+            maxViajero = max(viajeros)
+            print(f"El viajero con mas millas acumuladas es {maxViajero.getApellido()}, {maxViajero.getNombre()}")
+        elif opcion==5:
+            j=0
+            numV = int(input("Ingrese numero de viajero a acumular millas: "))
+            while j<=len(viajeros) and numV != viajeros[j].getNumero():
+                j+=1
+            if j > len(viajeros):
+                print("Numero incorrecto")
+            else:
+                mil = int(input("Ingrese la cantidad de millas a acumular: "))
+                viajeros[j] = viajeros[j] + mil
+                print("El viajero con numero {} ahora tiene {} millas".format(viajeros[j].getNumero(), viajeros[j].getMillas()))
+        elif opcion==6:
+            j=0
+            numV = int(input("Ingrese numero de viajero a canjear millas: "))
+            while j<=len(viajeros) and numV != viajeros[j].getNumero():
+                j+=1
+            if j > len(viajeros):
+                print("Numero incorrecto")
+            else:
+                mil = int(input("Ingrese la cantidad de millas a canjear: "))
+                viajeros[j] = viajeros[j] - mil
+                print("El viajero con numero {} ahora tiene {} millas".format(viajeros[j].getNumero(), viajeros[j].getMillas()))            
+        else:
+            print("Opción Inválida")
